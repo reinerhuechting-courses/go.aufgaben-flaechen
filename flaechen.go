539704704be5396e90bbb8054735d6f2b9af3be6
Reiner Hüchting <dev@reinerhuechting.de>
2022-12-15 01:19:07 +0100

    Lösungen entfernt.

--- flaechen.go
+++ flaechen.go
@@ -1,35 +1,38 @@
 package main
 
-import "math"
-
 // Erwartet zwei Seitenlängen a und b.
 // Liefert die Fläche des entsprechenden Rechtecks.
 func AreaRectangle(a, b float64) float64 {
-	return a * b
+	// TODO
+	return 0
 }
 
 // Erwartet eine Seitenlänge a.
 // Liefert die Fläche des entsprechenden Quadrats.
 func AreaSquare(a float64) float64 {
-	return AreaRectangle(a, a)
+	// TODO
+	return 0
 }
 
 // Erwartet zwei Seitenlängen a und b.
 // Liefert die Fläche eines rechtwinkligen Dreiecks, dessen Katheten a und b sind.
 func AreaRightTriangle(a, b float64) float64 {
-	return AreaRectangle(a, b) / 2
+	// TODO
+	return 0
 }
 
 // Erwartet zwei Seitenlängen a und b.
 // Liefert den Umfang des entsprechenden Rechtecks.
 func PerimeterRectangle(a, b float64) float64 {
-	return 2 * (a + b)
+	// TODO
+	return 0
 }
 
 // Erwartet eine Seitenlänge a.
 // Liefert den Umfang des entsprechenden Quadrats.
 func PerimeterSquare(a float64) float64 {
-	return PerimeterRectangle(a, a)
+	// TODO
+	return 0
 }
 
 // Erwartet die Längen der Katheten eines rechtwinkligen Dreiecks.
@@ -37,11 +40,13 @@ func PerimeterSquare(a float64) float64 {
 func Hypotenuse(a, b float64) float64 {
 	// Hinweis: Hierfür ist die Funktion
 	//          math.Sqrt() nützlich.
-	return math.Sqrt(a*a + b*b)
+	// TODO
+	return 0
 }
 
 // Erwartet zwei Seitenlängen a und b.
 // Liefert den Umfang eines rechtwinkligen Dreiecks, dessen Katheten a und b sind.
 func PerimeterRightTriangle(a, b float64) float64 {
-	return a + b + Hypotenuse(a, b)
+	// TODO
+	return 0
 }
